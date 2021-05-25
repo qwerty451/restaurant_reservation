182d2d59a6e9474fa5903184c5fb7c6f906af3df
tim <tvanheijst13@gmail.com>
2021-05-25 11:25:42 +0200

    Customer information
    Added all available customer information in table

--- klant.php
+++ klant.php
@@ -1,21 +1,44 @@
 <html>
     <head>
         <title>Bon Temps Klanten Overzicht</title>
+        <style>
+            table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            }
+        </style>
     </head>
     <body>
         <h2>Bons Temps - Klanten Overzicht</h2>
-        <p> Hier komen alle gegevens van de klanten.</p>
-        <a href="maak_klant.php">Nieuwe Klant</a>
+        <a href="reservering.php">Terug naar reservering</a><br><br>
+        <a href="maak_klant.php">Nieuwe Klant</a><br><br>
+        <p>Lijst van alle klanten</p>
+        <table style="width:80%">
+            <tr>
+                <th>id</th>
+                <th>Naam</th>
+                <th>Adres</th>
+                <th>Plaats</th>
+                <th>Nummer</th>
+                <th>Email</th>
+                <th>Postcode</th>
+            </tr>
+            <?php
+                include 'framework/database.php';
+                $klantenArray = $query->retrieve_all('klant'
+                );
+                foreach ($klantenArray as $klant) {
+                    echo "<tr>";
+                    echo "<td>" . $klant['id'] . "</td>";
+                    echo "<td>" . $klant['naam'] . "</td>";
+                    echo "<td>" . $klant['adres'] . "</td>";
+                    echo "<td>" . $klant['plaats'] . "</td>";
+                    echo "<td>" . $klant['nummer'] . "</td>";
+                    echo "<td>" . $klant['email'] . "</td>";
+                    echo "<td>" . $klant['postcode'] . "</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
     </body>
 </html>
-
-<?php
-    include 'framework/database.php';
-    $klantenArray = $query->retrieve_all('klant'
-    );
-    foreach ($klantenArray as $klant) {
-        echo "<br>";
-        echo $klant['naam'] . "  -  " . $klant['nummer'];
-    }
-    
-?>
